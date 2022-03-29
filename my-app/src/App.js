af3b760163c5426bde0d5dcb21d893875134e090
cenkberry <cenkberi@gmail.com>
2022-03-29 18:34:50 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,24 @@
 function App() {
-  return (
+   
+
     
-     <h1 className="bg-red-500 fac fjc h-44">Hey</h1>
+  return (
+       <div className="bg-gray-600 h-screen fac fjc flex-col">
+          
+          <div>Lorem, ipsum.</div>
+          <div>Similique, nulla.</div>
+          <div>Consequuntur, dolorem.</div>
+          <div>Pariatur, maxime!</div>
+          <div>Eum, voluptatibus.</div>
+          <div>Assumenda, vero?</div>
+          <div>Illo, sed?</div>
+          <div>Quidem, cumque?</div>
+          <div>Voluptatibus, voluptate?</div>
+          <div>Veniam, quo.</div>
+          <div>Placeat, aperiam.</div>
+          <div>Consequuntur, error.</div>
+ 
+     </div>
   );
 }
 
