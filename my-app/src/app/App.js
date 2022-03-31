a1cd5697080c42d8fcb08b4f2ef5096fcdee0132
cenkberry <cenkberi@gmail.com>
2022-03-31 16:35:55 +0300

    ok

---
+++ my-app/src/app/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import appclass from './appclass'
+
+function App() {
+
+
+  return(
+       <div className='h-screen bg-gray-600'>
+         
+       <header>
+           salam aleyküm
+       </header>
+          
+
+       </div>
+  )
+}
+
+export default App;
