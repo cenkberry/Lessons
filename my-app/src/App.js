22eb7d7ef851c79f2829ec9625289af8063fd443
cenkberry <cenkberi@gmail.com>
2022-04-04 11:34:02 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect , useRef , useState } from 'react';
 
 export default function App(){
+  
+   let box1  = React.useRef();
+   let box2  = React.useRef();
+   let box3  = React.useRef();
+
+   function runner(){
+      let x1 = box1.current.value;
+      let x2 = box2.current.value;
+
+     box3.current.textContent = +x1 + +x2;
+     box3.current.style.color = "green";
+   }
+
 
   return(
-    <div className="h-screen bg-gray-400 fac fjc">
-      
-      <div>s</div>
-     
+    <div className="h-screen bg-gray-400 p-20 space-y-3">
+       
+       <input type="text" ref={box1}/>
+       <br />
+       <input type="text" ref={box2}/>
+
+
+       <div className='fac fjc text-5xl' ref={box3}></div>
+
+       <button onClick={runner}>Click</button>
+
     </div>
   )
 }
