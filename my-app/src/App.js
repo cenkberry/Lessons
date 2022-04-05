bd9d9bed3dae30778dd4340ace26672f601a9ff0
cenkberry <cenkberi@gmail.com>
2022-04-05 13:09:46 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,30 @@
-import React, { useEffect , useRef , useState } from 'react';
+import React, { useEffect , useRef , useState , useReducer } from 'react';
 
+export default function App(){
+  function reducer(state , action){
 
-export default function App(props){
+    switch(action.type){
+      case "boy" : return {name: "John"}
+      case "girl" : return {name: "Cecilla"}
+      default : return {name: "default"}
+    }
+ }
+  const [state , dispatch] = useReducer(reducer , { name: "default"});
    
+  function boyname(){
+    dispatch({type: "boy"})
+  }
+  
+  function girlname(){
+    dispatch({type: "girl"})
+  }
   
   return(
-    <div className="h-screen  bg-gray-400 fac fjc flex-col">
-
+    <div className="h-screen bg-gray-400 fac fjc">
+       
+        <div className="mr-10">{state.name}</div>
+        <div onClick={boyname} className="btn btn-black">boy</div>
+        <div onClick={girlname} className="btn btn-yellow">girl</div>
     </div>
   )
 }
