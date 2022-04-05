9010a06f6d1c2dce2a015474a4657e340d730510
cenkberry <cenkberi@gmail.com>
2022-04-05 07:49:04 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,15 @@
 import React, { useEffect , useRef , useState } from 'react';
-import Livediv from './Livediv';
-import lives from './lives';
+
 
 export default function App(){
 
-   const artists = lives.map(artist => {
-        
-      return(
-        <Livediv 
-        key={artist.id}
-        name={artist.name}
-        place={artist.place}
-        ticket={artist.ticket} 
-        />
-      )  
-   });
+  
 
 
   return(
-    <div className="h-screen p-5 bg-gray-400 grid grid-cols-[1fr_1fr]">
+    <div className="h-screen  bg-gray-400 fac fjc">
        
-      {artists}
+      
     
     </div>
   )
