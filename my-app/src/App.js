2424ad4c91361662ab0e4cd6b8d8573ff6646a84
cenkberry <cenkberi@gmail.com>
2022-04-06 16:12:23 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,42 @@
-import React, { useEffect , useRef , useState , useReducer } from 'react';
+import React, { useEffect , useRef , useState , useReducer , Component } from 'react';
 
-export default function App(){
+class App extends Component{
+    constructor(props) {
+      super(props);
+    }
+    
+    state = {
+          
+      isVis : false,
+      butStat: true
+    }
 
-  
-  return(
-    <div className="h-screen bg-gray-400">
-       <div>ok</div>
-    </div>
-  )
+
+    showHide = (e)=>{
+        this.setState({
+          isVis : !this.state.isVis,
+          butStat: !this.state.butStat
+        })
+    }
+
+
+  render(){
+
+     const {isVis} = this.state;
+     const {butStat} = this.state;
+     
+    return(
+      <div className="fac fjc">
+        <div className="grid grid-cols-[5fr_1fr] bg-blue-300 p-2">
+          <p className="bg-red-300 p-2">Temporibus?</p>
+          <button onClick={this.showHide}
+          className="btn btn-green">{butStat ? "Show" : "Hide"}</button>
+          {isVis && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea?</p>}
+        </div>
+
+      </div>
+    )
+  }
 }
+
+export default App;
