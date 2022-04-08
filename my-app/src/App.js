d5f9aa77ac14cbfa80b20b014336aad570c55e88
cenkberry <cenkberi@gmail.com>
2022-04-08 16:56:07 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import Store from './Context';
-import Comp1 from './comp/Comp1';
-import Comp2 from './comp/Comp2';
-import Comp3 from './comp/Comp3';
 
 export default function App(){
      
     return(
-      <Store>
-        <div className="h-screen bg-gray-500 fac fjc flex-row space-x-4"> 
-          <Comp1 />
-          <Comp2 />
-          <Comp3 />
+        <div className="h-screen bg-gray-500 fac fjc flex-col"> 
+           
+        <div>Lorem ipsum dolor sit.</div>
+        <div>Dolorem distinctio nulla expedita.</div>
+        <div>Delectus earum possimus mollitia!</div>
+        
         </div>
-      </Store>
     )
 }
