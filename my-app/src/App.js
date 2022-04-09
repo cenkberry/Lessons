4c09ac7599662587493298667a92f65d7443c8af
cenkberry <cenkberi@gmail.com>
2022-04-09 13:03:52 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,10 +5,10 @@ export default function App(){
     return(
         <div className="h-screen bg-gray-500 fac fjc flex-col"> 
            
-        <div>Lorem ipsum dolor sit.</div>
-        <div>Dolorem distinctio nulla expedita.</div>
-        <div>Delectus earum possimus mollitia!</div>
-        
+           <div>Lorem ipsum dolor sit.</div>
+           <div>Earum, ratione? Ipsa, modi?</div>
+           <div>Perferendis voluptates maxime deserunt!</div>
+
         </div>
     )
 }
