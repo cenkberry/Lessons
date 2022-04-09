e1750096bc6430c6c97067a8f6841b15ccdd0a48
cenkberry <cenkberi@gmail.com>
2022-04-09 16:49:20 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 export default function App(){
 
+
      
     return(
-        <div className="h-screen bg-indigo-400 fac fjc flex-col"> 
-           
-           <div>Earum, ratione? Ipsa, modi?</div>
-           <div>Perferendis voluptates maxime deserunt!</div>
-    
+        <div className="h-screen bg-indigo-400"> 
+          
         </div>
     )
 }
