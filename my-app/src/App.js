8f94f768ed46eff3286e0dca3717ecea7e2a2a57
cenkberry <cenkberi@gmail.com>
2022-04-10 15:34:45 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 export default function App(){
 
-
-     
+  
+  
+   
     return(
-        <div className="h-screen bg-indigo-400"> 
+        <div className="h-screen bg-indigo-400 fac fjc flex-col"> 
           
+        
         </div>
     )
 }
