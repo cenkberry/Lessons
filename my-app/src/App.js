5ae74d437a3bae95e734260bade7ab795ecec60e
cenkberry <cenkberi@gmail.com>
2022-04-12 18:43:31 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
-import React,{useState} from 'react';
+import React,{useState , useRef} from 'react';
 
 export default function App() {
 
+
+
+
   return (
     <div className="relative h-screen fac fjc flex-col bg-gray-300 page">
-       <div>test</div>
+      
     </div>
   )
 }
