5ff85acf9c71fafe7d9026b6762d77e3b752fb42
cenkberry <cenkberi@gmail.com>
2022-04-16 15:17:08 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,8 @@ export default function App() {
 
 
   return (
-    <div className="relative h-screen fac fjc flex-col bg-gray-300 page">
-      
+    <div className="h-screen fac fjc flex-col bg-gray-300 page">
+      ok
     </div>
   )
 }
