e01fbedce8c714dafc5838d8f66a9f92c2be2786
cenkberry <cenkberi@gmail.com>
2022-04-30 15:00:26 +0300

    new practices

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,32 @@
-import React, {useState , useRef} from 'react';
+import React,{useState} from 'react';
 
 export default function App() {
 
+   const [myarr , setArr] = useState([]);
+   const [inputItem , setInput] = useState("");
 
+   function Additem(){
+     setArr(myarr => [...myarr, inputItem])
+   }
 
+   function clearItems(){
+     setArr([])
+   }
 
-  return (
-    <div className="h-screen fac fjc flex-col bg-gray-300 page">
-      ok
+  return(
+    <div className="h-screen fac fjc flex-col bg-gray-300">
+    {
+       myarr.map((todo,index)=>{
+         return(
+           <div key={index} className="bg-red-300 p-3 my-3">
+            {todo}
+           </div>
+         )
+       })
+     }
+     <input type="text" onClick={(e)=>{e.target.value = ""}} onChange={(e)=>{setInput(e.target.value)}} />
+     <button className="btn btn-green" onClick={Additem}>Add</button>
+     <button className="btn btn-red" onClick={clearItems}>Clear Items</button>
     </div>
   )
 }
