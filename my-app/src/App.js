1f4587b95e42c118a95c9b2b6f6a922e01ffe7bd
cenkberry <cenkberi@gmail.com>
2022-05-03 17:06:33 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,10 @@
-import React from 'react';
 
 function App() {
-
 
 
   return(
      <div className='h-screen fac fjc flex-col bg-gray-200'>
 
-       <div>aaa</div>
-       <div>sss</div>
-       <div>333</div>
 
      </div>
   )
---
+++ my-app/src/Users.js
@@ -0,0 +1,24 @@
+const Users = [
+  {
+    id: 1,
+    name: "Cenk",
+    surname: "Beri"
+  },
+  {
+    id: 2,
+    name: "Rana",
+    surname: "Beri"
+  },
+  {
+    id: 3,
+    name: "Osman",
+    surname: "Beri"
+  },
+  {
+    id: 4,
+    name: "Hamdi",
+    surname: "Beri"
+  }
+];
+
+export default Users;
