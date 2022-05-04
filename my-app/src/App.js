074aa505fd1093465f820f216cf574156daab8f2
cenkberry <cenkberi@gmail.com>
2022-05-04 19:04:04 +0300

    ok

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React,{useRef} from 'react';
 
 export default function App() {
 
-
+ 
 
   return (
     <div className="h-screen fac fjc flex-col bg-gray-300">
         <div>Lorem ipsum dolor sit amet.</div>
-        <div>Reiciendis saepe repellat iste modi!</div>
+        <div >Reiciendis saepe repellat iste modi!</div>
         <div>Odio doloribus perferendis ea vitae!</div>
+        <div className="btn btn-green">Click</div>
     </div>
   );
 }
